a9cf39449a5292b0d3337dcbbe689d314a5887ec
Martin Leiza <martin.leiza@gmail.com>
2025-09-05 13:49:40 -0300

    Agrego lógica al método buscarTelefono()

--- Tp5_g1/src/Clases/Directorio.java
+++ Tp5_g1/src/Clases/Directorio.java
@@ -15,66 +15,69 @@ import java.util.*;
  * @author arceb
  */
 public class Directorio {
-     private long telefono;
-    
+
+    private long telefono;
+
     private static TreeMap<Long, Contactos> informacion = new TreeMap<>();
-    
+
     //permite registrar un nuevo contacto con su respectivo nro. de teléfono. Siendo el nro. del teléfono la clave del mismo.
-    public static boolean agregarContacto(long telefono, Contactos cont){
+    public static boolean agregarContacto(long telefono, Contactos cont) {
         //verificamos q telefono sea un numero valido y el contacto no sea null
-        if (telefono <=0 || cont == null){
+        if (telefono <= 0 || cont == null) {
             return false;
         }
-        
+
         //verificamos q el telefono no este duplicado
-        
-        if(informacion.containsKey(telefono)){
+        if (informacion.containsKey(telefono)) {
             return false;
         }
-        
+
         informacion.put(telefono, cont);
-        
+
         return true;
-        
-        
+
     }
-    
-    public static Set <Long> todosLosTelefonos (){
+
+    public static Set<Long> todosLosTelefonos() {
         return new TreeSet<>(informacion.keySet());
     }
+
     //en base al nro. de teléfono retorna el Contacto asociado al mismo.
-    public Contactos buscarContacto(long telefono){
-           //nos devuelve el contacto con respecto al telefono asociado
+    public Contactos buscarContacto(long telefono) {
+        //nos devuelve el contacto con respecto al telefono asociado
         return informacion.get(telefono);
-        
+
     }
-    
+
     // en base a un apellido nos devuelve un Set<Long> con los números de teléfono asociados a dicho apellido.
-    public void buscarTelefono(){
-        
-    
+    public Set<Long> buscarTelefono(String apellido) { 
+        TreeSet<Long> numeroEncontrado = new TreeSet<>(); //Creo un TreeSet para almacenar coincidencias
+        for (Map.Entry<Long, Contactos> c : informacion.entrySet()) { //Recorro el TreeMap información
+            if (c.getValue().getApellido().equalsIgnoreCase(apellido)) { //Comparo lo que recibo por parámetro
+                numeroEncontrado.add(c.getKey()); //Si hay coincidencia lo agrego al TreeSet
+            }
+        }
+        return numeroEncontrado; //Retorno lo que almacene en el TreeSet
     }
-    
+
     //en base a una ciudad nos devuelve un ArrayList con los Contactos asociados a dicha ciudad.
-    public void buscarContactos(){
-    
-    //en base al número de teléfono elimina el contacto del directorio.   
+    public void buscarContactos() {
+
+        //en base al número de teléfono elimina el contacto del directorio.   
     }
-    
-    public static Set<Integer> TodosLosDni(){
+
+    public static Set<Integer> TodosLosDni() {
         Set<Integer> listaDni = new TreeSet<>();
-        
+
         for (Contactos dni : informacion.values()) {
             listaDni.add(dni.getDni());
         }
         return listaDni;
     }
-    
-    
-    
-    public TreeMap<Long, Contactos> borrarContacto(int dni){
-       informacion.containsValue(dni);
-        
-       return informacion;
+
+    public TreeMap<Long, Contactos> borrarContacto(int dni) {
+        informacion.containsValue(dni);
+
+        return informacion;
     }
 }
